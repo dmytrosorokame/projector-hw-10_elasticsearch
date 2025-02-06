dbcbd6aa9a98b6c066029a7172176531fb436337
Dmytro Soroka <dmytro.soroka@betterme.world>
2025-02-06 20:30:02 +0100

    feat: finished homework

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# projector-hw-10_elasticsearch
+# Projector HSA Home work #10: Elasticsearch
+
+To Start the project, run the following command:
+
+```bash
+docker compose up --build
+```
+
+Wait for the project to start.
+
+To Create the index, you need to run the following command:
+
+```bash
+curl -X POST http://localhost:8000/elasticsearch
+```
+
+You can check the index creation status by visiting `http://localhost:5601/app/management/data/index_management/indices`
+
+To Check suggestions, you can visit Web Page on `http://localhost:80` and use the search input.
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,70 @@
+import { useCallback, useState } from "react";
+
+const API_URL = "http://localhost:8000";
+
+const fetchSuggestions = async (query: string) => {
+  const response = await fetch(`${API_URL}/autocomplete?query=${query}&size=5`);
+  const data = await response.json();
+
+  return data.suggestions;
+};
+
+const debounce = <T extends unknown[]>(
+  func: (...args: T) => void,
+  delay: number
+) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: T) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
+
 function App() {
-  return <div>React app</div>;
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+
+  const handleFetchSuggestions = async (query: string) => {
+    const suggestions = await fetchSuggestions(query);
+    setSuggestions(suggestions || []);
+  };
+
+  const handleFetchDebounced = useCallback(
+    debounce(handleFetchSuggestions, 300),
+    []
+  );
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+
+    setInput(query);
+
+    handleFetchDebounced(query);
+  };
+
+  return (
+    <div>
+      <div>
+        Search: <input type="text" value={input} onChange={handleInputChange} />
+      </div>
+
+      <h2>Suggestions</h2>
+
+      {!suggestions.length && <p>No suggestions.</p>}
+
+      {suggestions.length > 0 ? (
+        <ul>
+          {suggestions?.map((suggestion: string) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
